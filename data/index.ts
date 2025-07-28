e1e74c0c6ffcbd57c6bb79601fd4f5f0d942a30e
Wayne <waynem567@gmail.com>
2025-07-28 19:54:41 +0200

    Update my description

--- data/index.ts
+++ data/index.ts
@@ -59,7 +59,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: "Do you want to start a project together?",
+    title: "Want to start a project together?",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
