4d3f1e6830b19625568438b11bd07c964ad4be2a
Wayne <waynem567@gmail.com>
2025-08-10 19:11:49 +0200

    Comments

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -4,6 +4,20 @@ import * as Sentry from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect } from "react";
 
+/**
+ * The global error page component.
+ *
+ * This component is responsible for rendering the error page for all app routes.
+ * It receives an `error` prop with an `Error` object that contains information
+ * about the error that occurred.
+ *
+ * The component uses the `useEffect` hook to capture the error with Sentry.
+ * The `digest` property of the error object can be used to identify the error
+ * in Sentry.
+ *
+ * The component returns a JSX element representing the error page. The page
+ * renders a generic error message using the `NextError` component from Next.js.
+ */
 export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
     Sentry.captureException(error);
