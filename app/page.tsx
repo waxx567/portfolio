24c4697a4201ed653bc2350a94a8f4a0bcb18eb9
Wayne <waynem567@gmail.com>
2025-08-10 19:10:32 +0200

    Comments

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,15 @@ import Footer from "@/components/Footer";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 
+/**
+ * The main entry point of the website.
+ *
+ * This component renders the full website page, including the hero section,
+ * the grid section, the recent projects section, the clients section, the
+ * experience section, the approach section, and the footer section.
+ *
+ * @returns The main entry point of the website.
+ */
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
