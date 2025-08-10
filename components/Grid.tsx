932471857d1ba40a04f9545e3051cdb2dd658bb1
Wayne <waynem567@gmail.com>
2025-08-10 19:07:09 +0200

    Comments

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,6 +1,16 @@
 import { gridItems } from "@/data";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
+/**
+ * A React component that renders a section containing a grid of items.
+ *
+ * This component uses the BentoGrid and BentoGridItem components to display
+ * a collection of items defined in the gridItems array.
+ * Each item in the grid displays a title, description, image, and optional
+ * styling classes.
+ *
+ * @returns A section element with a grid of BentoGridItems.
+ */
 const Grid = () => {
   return (
     <section id="about">
